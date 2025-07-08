220e86a0d79dedd0d0a0a87f13de333a6b45c5f1
Jennifer_Zheng <168129525+Qqing0531@users.noreply.github.com>
2025-07-07 18:17:59 -0700

    Update client.ts

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,18 +1,21 @@
-import { initializeApp,getApp, getApps } from "firebase/app";
-import { getAuth } from 'firebase/auth';
-import {getFirestore} from "firebase/firestore";
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: "ai-mock-interviews-69ce0.firebaseapp.com",
-    projectId: "ai-mock-interviews-69ce0",
-    storageBucket: "ai-mock-interviews-69ce0.firebasestorage.app",
-    messagingSenderId: "500631745410",
-    appId: "1:500631745410:web:df288d5b59b680fed8959b",
-    measurementId: "G-BZ257NN9R9"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
